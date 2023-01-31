f414e1a4df1b438ffb633b7cc88377b2cb47e22b
Rolf Häckel <rolf.haeckel@mac.com>
2023-01-31 10:52:40 +0100

    initialisiert

---
+++ sendBack.php
@@ -0,0 +1,6 @@
+<?php
+// dieser Text soll in responesText geschrieben werden
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+
+echo "Dein voller Name lautet $firstName $lastName";
